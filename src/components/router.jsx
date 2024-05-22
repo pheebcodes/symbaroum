976db88abae2d5d9c4b42e9cea49055cefd35ecc
phoebe <me@phoebe.codes>
2024-05-22 15:43:34 -0400

    Update redux, fix warning/error in router

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -3,13 +3,25 @@ import { ScrollToTop } from "./scroll_to_top.jsx";
 import { App } from "./app.jsx";
 import { PrintSheet } from "./print_sheet.jsx";
 
+function WrappedPrintSheet(props) {
+  return <PrintSheet {...props} />;
+}
+
+function WrappedApp(props) {
+  return <App {...props} />;
+}
+
 export function Router() {
   return (
     <BrowserRouter>
       <ScrollToTop>
         <Switch>
-          <Route exact path="/character/:id/print" component={PrintSheet} />
-          <Route path="/" component={App} />
+          <Route
+            exact
+            path="/character/:id/print"
+            component={WrappedPrintSheet}
+          />
+          <Route path="/" component={WrappedApp} />
         </Switch>
       </ScrollToTop>
     </BrowserRouter>
--- src/store/index.js
+++ src/store/index.js
@@ -1,43 +1,49 @@
-import {Map, List} from "immutable";
-import {applyMiddleware, createStore} from "redux";
-import thunk from "redux-thunk";
-import {LocalStorageBacked} from "./local_storage_backed.js";
-import {messages} from "../data/index.js";
+import { Map, List } from "immutable";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import { thunk } from "redux-thunk";
+import { LocalStorageBacked } from "./local_storage_backed.js";
+import { messages } from "../data/index.js";
 import {
   UPDATE_CHARACTERS,
   SHOW_MODAL,
   HIDE_MODAL,
   READ_MESSAGES,
   SET_MESSAGE,
-  SET_PAGE_SIZE
+  SET_PAGE_SIZE,
 } from "./action_consts";
 
-const defaultState = Map({characters: List()});
+const defaultState = Map({ characters: List() });
 
 function reducer(state = {}, action) {
   switch (action.type) {
-  case UPDATE_CHARACTERS:
-    return state.setIn(["characters"].concat(action.keyPath), action.value);
-  case SHOW_MODAL:
-    return state.set("modal",
-      Map({message: action.message, action: action.action, cb: action.cb}));
-  case HIDE_MODAL:
-    if (action.action) {
-      return reducer(state, action.action).delete("modal");
-    }
-    return state.delete("modal");
-  case READ_MESSAGES:
-    return state.set("readMessages", messages.length);
-  case SET_MESSAGE:
-    return state.set("currentMessage", action.message);
-  case SET_PAGE_SIZE:
-    return state.set("pageSize", action.pageSize);
-  default:
-    return state;
+    case UPDATE_CHARACTERS:
+      return state.setIn(["characters"].concat(action.keyPath), action.value);
+    case SHOW_MODAL:
+      return state.set(
+        "modal",
+        Map({ message: action.message, action: action.action, cb: action.cb })
+      );
+    case HIDE_MODAL:
+      if (action.action) {
+        return reducer(state, action.action).delete("modal");
+      }
+      return state.delete("modal");
+    case READ_MESSAGES:
+      return state.set("readMessages", messages.length);
+    case SET_MESSAGE:
+      return state.set("currentMessage", action.message);
+    case SET_PAGE_SIZE:
+      return state.set("pageSize", action.pageSize);
+    default:
+      return state;
   }
 }
 
-const localStorage = LocalStorageBacked("state", defaultState, ["characters", "readMessages", "pageSize"]),
+const localStorage = LocalStorageBacked("state", defaultState, [
+    "characters",
+    "readMessages",
+    "pageSize",
+  ]),
   middleware = applyMiddleware(localStorage.middleware, thunk);
 
 export const store = createStore(reducer, localStorage.initial(), middleware);
