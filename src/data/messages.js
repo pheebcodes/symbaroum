36db065abeb2c852d193d2acfa9aa6730a36d148
phoebe <me@phoebe.codes>
2024-05-22 10:57:02 -0400

    Update message

--- src/data/messages.js
+++ src/data/messages.js
@@ -1,11 +1,10 @@
 export const messages = [
   {
     title: "Welcome!",
-    message: "Thank you for using my Symbaroum character creator! All data is \
-      automatically saved, so you can close the tab or return to the menu from \
-      the editor at any time. If you have \
-      any issues, email me at <a href=\"email:me@chncdcksn.com\">me@chncdcksn.com</a>\
-      or create an issue <a href=\"https://github.com/chncdcksn/symbaroum\">here</a>\
-      and I'll be sure to fix it as soon as possible. :)"
-  }
+    message:
+      "Thank you for using my Symbaroum character creator! All data is \
+      automatically saved, but is potentially ephemeral, so be sure to save backups of your \
+      characters. If you have \
+      any issues, please create an issue <a href=\"https://github.com/pheebcodes/symbaroum\">here</a>. :)",
+  },
 ];
